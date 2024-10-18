9610cde93863a40f5b82adb877ba368826ad136a
MigoiArmando <ashurasyntax@gmail.com>
2024-10-18 20:53:46 +0800

    update landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,27 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
+import logo from "./images/Ateneo_de_Naga_University_logo.png";
 import "./App.css";
+import "./SignupPage.css"; 
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="login-container">
+    <div className="outer-container">
+      <div className="logo">
+        <img src={logo} alt="Logo" className="logo-image" />
       </div>
-      <h1>Parakito Si Mark</h1>
-    </>
+      <h2>Log In</h2>
+      <p>Don't have an account? <a href="#">Sign Up</a></p>
+      <div className="buttons">
+        <button className="btn teacher">Sign Up as Teacher</button>
+        <button className="btn student">Sign Up as Student</button>
+      </div>
+    </div>
+    <footer>
+      <a href="#">Terms of Use</a>
+      <a href="#">Privacy Policy</a>
+      <p>This site is protected by Kwatro Kantos. Privacy Policy and Terms of Use apply.</p>
+    </footer>
+  </div>
   );
 }
 
