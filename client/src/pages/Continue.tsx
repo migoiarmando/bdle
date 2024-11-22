77a1eed45fb7858c6573be3ba4b0a351550bdc41
MigoiArmando <ashurasyntax@gmail.com>
2024-11-22 09:29:34 +0800

    Added continue page

---
+++ client/src/pages/Continue.tsx
@@ -0,0 +1,36 @@
+import logo from "../assets/images/Ateneo_de_Naga_University_logo.png";
+import { useNavigate } from "react-router-dom";
+import "./../styles/App.css";
+import Footer from "../components/Footer";
+
+function App() {
+  const navigate = useNavigate(); //
+  return (
+    <div className="login-container">
+      <div className="outer-container">
+        <div className="adnu-logo">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </div>
+        <h2 className="lg-header-2">Welcome to BDLE</h2>
+
+        <div className="buttons">
+          <button onClick={() => navigate("/login")} className="btn teacher">
+            Sign In as Teacher
+          </button>
+          <button
+            className="btn student"
+            onClick={() => navigate("/student-form")}
+          >
+            Sign Up as Student
+          </button>
+        </div>
+        <p className="lg-p" onClick={() => navigate("/login")}>
+          Don't have an account? <a href="#">Sign In</a>
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
