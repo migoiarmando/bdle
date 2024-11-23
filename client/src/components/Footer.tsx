f060b26ff17c6683b937664fa93c6dee72527703
MigoiArmando <ashurasyntax@gmail.com>
2024-11-23 08:03:54 +0800

    update

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Footer: React.FC = () => {
   return (
     <div className="footer">
-    <a href="#" className="footer-link" style={{marginRight:'10px'}}>Terms of Use</a>
-    <a href="#" className="footer-link">Privacy Policy</a>
-    <p>
-      This site is protected by Kwatro Kantos.{' '}
-      <a href="#">Privacy Policy</a> and <a href="#">Terms of Use apply</a>
-    </p>
+      <a href="#" className="footer-link" style={{ marginRight: "10px" }}>
+        Terms of Use
+      </a>
+      <a href="#" className="footer-link">
+        Privacy Policy
+      </a>
+      <p>
+        This site is protected by Kwatro Kantos. <a href="#">Privacy Policy</a>{" "}
+        and <a href="#">Terms of Use apply</a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
