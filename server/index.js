835e0aecdbe904d15f5900ed7b3a2ea426f186d4
MigoiArmando <117260354+migoiarmando@users.noreply.github.com>
2024-11-25 12:44:05 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import connectToMongoDb from "./db/connectToMongoDb.js";
 import apiRoute from "./routes/api.route.js";
+import path from "path";
 
 /** Initialization */
 config();
@@ -16,6 +17,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", apiRoute());
+
+/** Connect the Frontend */
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectToMongoDb();
