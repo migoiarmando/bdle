a133c40f430e3fea6ada3d11b7b1e8f01b55acf3
MigoiArmando <ashurasyntax@gmail.com>
2024-11-27 00:09:11 +0800

    frontend fixes

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 import "../styles/notFound.css";
-import notFoundImg from '../assets/not-found.svg'
+import notFoundImg from "../assets/not-found.svg";
 
 const NotFound: React.FC = () => {
   return (
     <div className="content">
-        <img src={notFoundImg} alt="" />
-        <h3>Page not found</h3>
-        <span className='notfound-content'>We're working hard to get it ready for you. Stay tuned for updates!</span>
+      <img src={notFoundImg} alt="" />
+      <h3>Page not found</h3>
+      <span className="notfound-content">
+        We are working hard to get it ready for you. Stay tuned for updates!
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
