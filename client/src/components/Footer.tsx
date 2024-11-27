04312e7f920de0dfd22c9fb0c8ed203a82d39d8b
MigoiArmando <ashurasyntax@gmail.com>
2024-11-27 23:30:42 +0800

    Update Footer.tsx

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,10 +4,7 @@ const Footer: React.FC = () => {
   return (
     <div className="footer">
       <a href="#" className="footer-link" style={{ marginRight: "10px" }}>
-        Terms of Use
-      </a>
-      <a href="#" className="footer-link">
-        Privacy Policy
+        About us
       </a>
       <p>
         This site is protected by Kwatro Kantos. <a href="#">Privacy Policy</a>{" "}
